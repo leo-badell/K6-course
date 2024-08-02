ca6b6c2ee0575e85308b1b5b6b19259f69fb3b4c
Leonardo Badell <leonardobadell@hotmail.com>
2024-08-02 20:38:44 -0300

    Adding lattest assiggment

---
+++ BuildingHTTPRequest/post.assigment.js
@@ -0,0 +1,82 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+
+    const credentials = {
+        username: 'test_' + Date.now(),
+        password: 'secret_' + Date.now(),
+    }
+
+    http.post(
+        'https://test-api.k6.io/user/register/',
+        JSON.stringify(credentials),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+    let res = http.post(
+        'https://test-api.k6.io/auth/token/login/',
+        JSON.stringify(
+            {
+                username: credentials.username,
+                password: credentials.password
+            }
+        ),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+
+    const accessToken = res.json().access;
+    console.log(accessToken);
+
+    res = http.get(
+        'https://test-api.k6.io/my/crocodiles/',
+        {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+    );
+
+
+    res = http.post(
+        'https://test-api.k6.io/my/crocodiles/',
+        JSON.stringify(
+            {
+                name: 'Fulano el cocodrilo',
+                sex: 'M',
+                date_of_birth: '2000-10-10'
+            }
+        ),
+        {
+            headers: {
+                Authorization: 'Bearer ' + accessToken,
+                'Content-Type': 'application/json'
+            }
+        }
+    );    
+
+    const crocodrileId = res.json().id;
+
+   res = http.get(`https://test-api.k6.io/my/crocodiles/${crocodrileId}`,
+        {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+    ) 
+    
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'crocodrileId': (r) => r.json().id === crocodrileId
+    })
+}
+    
